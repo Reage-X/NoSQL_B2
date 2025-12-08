3bab8b87511159ff7d1e44fbaa69c2772052d468
Reage-X <sergekshyvnyak@gmail>
2025-12-09 00:58:34 +0100

    Add newline at end of UpdateUserName.js and UserNameExist.js files

--- Event.java
+++ Event.java
@@ -1,20 +1,39 @@
+import java.time.LocalDateTime;
+
 public class Event {
-    private String Titre;
-    private String png_vid;
+    private String id;
+    private String title;
+    private String url;
     private String description;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String location;
+    private String organisateurId;
     
-    public Event(String Titre, String png_vid, String description) {
-        this.Titre = Titre;
-        this.png_vid = png_vid;
+    public Event(String title, String description, LocalDateTime startDate, String location) {
+        this.title = title;
         this.description = description;
+        this.startDate = startDate;
+        this.location = location;
     }
     public Event() {}
     
-    public String getTitre()                            { return Titre; }
-    public String getPng_vid()                          { return png_vid; }
-    public String getDescription()                      { return description; }
+    public String getId()                                   { return id; }
+    public String getTitle()                                { return title; }
+    public String getUrl()                                  { return url; }
+    public String getDescription()                          { return description; }
+    public LocalDateTime getStartDate()                     { return startDate; }
+    public LocalDateTime getEndDate()                       { return endDate; }
+    public String getLocation()                             { return location; }
+    public String getOrganisateurId()                       { return organisateurId; }
     
-    public void setDescription(String description)      { this.description = description; }
-    public void setTitre(String Titre)                  { this.Titre = Titre; }
-    public void setPng_vid(String png_vid)              { this.png_vid = png_vid; }
+
+    public void setId(String id)                            { this.id = id; }
+    public void setTitle(String title)                      { this.title = title; }
+    public void setUrl(String url)                          { this.url = url; }
+    public void setDescription(String description)          { this.description = description; }
+    public void setStartDate(LocalDateTime startDate)       { this.startDate = startDate; }
+    public void setEndDate(LocalDateTime endDate)           { this.endDate = endDate; }
+    public void setLocation(String location)                { this.location = location; }
+    public void setOrganisateurId(String organisateurId)    { this.organisateurId = organisateurId; }
 }
